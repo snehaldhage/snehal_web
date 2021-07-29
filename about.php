970a0c294d8c2c9df8bbb32846e0fd030e0172b8
snehaldhage <snehaldhage19199@gmail.com>
2021-07-28 23:06:38 -0400

    Initial commit

---
+++ about.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>India</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"> 
+  </head>
+<body>
+	<!-- Navbar-->
+	<nav class="navbar navbar-expand-lg navbar-dark bg-dark"style="background-color: #e3f2fd";>
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#" style="font-size: 300%"><strong>It's All About India</strong></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav ml-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Gallery</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Places</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">About Us</a>
+        </li>
+         </ul>
+      <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+
+<div class="jumbotron">
+  <h1>The most beautiful places in india</h1>
+  <p>A travellers' list is a complicated place. It includes places that are world heritage sites and then it has places no one has ever heard of, it has places that every travel guide book talks about and then it has places barely visible on Google Maps. To add to the marvels of every travellers must visit list, I present a list of 10 Most Beautiful Places in The World, that are in in India and are a must visit.
+
+</p>
+<a href="index.html" class="btn btn-outline-success">Home Page</a>
+</div>
+</body>
+</html>
